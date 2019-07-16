97460dc671ae9312c035d32a9add71c9710bed0f
Cat <catalinajuarros@gmail.com>
2019-07-16 20:37:23 -0300

    Write ASM code for Blowfish round and delete binary

--- src/bcrypt.asm
+++ src/bcrypt.asm
@@ -8,6 +8,7 @@ extern initstate_asm
 global blowfish_init_state_asm
 global blowfish_expand_state_asm
 global f_asm
+global blowfish_round_asm
 ; global bcrypt_encrypt
 
 section .data
@@ -35,10 +36,10 @@ section .text
 ; with an 8-bit register instead of using & 0xff
 
 ; Function for Feistel network
-; %1 -> array of S-box
+; %1 -> array of S-boxes
 ; %2: right half of data block, x
-; %3: temporary register for shifting x
-; %4: output
+; %3: temporary register for shifting x (modified)
+; %4: output (modified)
 %macro F 4
     ; %4 <- S[0][x >> 24] + S[1][x >> 16 & 0xff]
     mov %3, %2
@@ -64,6 +65,19 @@ section .text
     add %4, [%1 + %3*S_ELEMENT_MEMORY_SIZE]
 %endmacro
 
+; %1 -> array of S-boxes
+; %2 -> P-array
+; %3: Xr, output (modified)
+; %4: Xl
+; %5: P-array index
+; %6: temporary register for F (modified)
+; %7: temporary register for F output (modified)
+%macro BLOWFISH_ROUND 7
+    F %1, %3, %6, %7
+    xor %7, [%2 + %5*P_VALUE_MEMORY_SIZE]
+    xor %3, %7
+%endmacro
+
 ; Intended exclusively for testing Feistel function
 ; uint32_t f_asm(uint32_t x, blf_ctx *state)
 f_asm:
@@ -74,6 +88,24 @@ f_asm:
     mov  rbp, rsp
 
     F rsi, rdi, rdx, rax
+
+    pop rbp
+    ret
+
+; Intended exclusively for testing Blowfish round
+; uint32_t blowfish_round_asm(uint32_t xl, uint32_t xr, blf_ctx *state,
+;                             uint32_t n)
+blowfish_round_asm:
+    ; rdi: left half of data block, Xl
+    ; rsi: right half of data block, Xr
+    ; rdx -> Blowfish state (array of S-boxes and P-array)
+    ; rcx: P-array index
+    push rbp
+    mov  rbp, rsp
+
+    mov  r8, [rdx + BLF_CTX_P_OFFSET]
+    BLOWFISH_ROUND rdx, r8, rsi, rdi, r8, r9, r10
+    mov rax, rsi
 
     pop rbp
     ret
