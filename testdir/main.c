66a395512dfb113dcb701c84226812a7f6a19344
Cat <catalinajuarros@gmail.com>
2019-07-22 12:06:53 -0300

    Clean up code

--- testdir/main.c
+++ testdir/main.c
@@ -11,33 +11,6 @@
 #include "../src/bcrypt.h" // TODO: figure out how to make "bcrypt.h" work
 #include "../src/bcrypt_constants.h"
 
-// TODO: replace "initial_state" with a parameter
-// TODO: refactor these two functions
-// void do_test(uint32_t actual, uint32_t expected, const char *test_name,
-//              const char *args_format, ...)
-// {
-//     // Construct args string
-//     char out[4096];
-//     va_list args;
-//     va_start(args, args_format);
-//     vsnprintf(out, sizeof(out), args_format, args);
-
-//     // Print test info: "Running test test_name with arguments args"
-//     fprintf(stdout, "\n");
-//     fprintf(stdout, "\033[1;35m");
-//     fprintf(stdout, "Running test %s with arguments ", test_name);
-//     fprintf(stdout, "%s\n", out);
-//     fprintf(stdout, "\033[0m");
-
-//     // Test
-//     if (actual == expected) {
-//         test_pass("%s successful.\n", test_name);
-//     } else {
-//         test_fail("%s failed.\n"
-//         "Expected: 0x%08x\tActual: 0x%08x\n", test_name, expected, actual);
-//     }
-// }
-
 void do_test(uint64_t actual, uint64_t expected, const char *test_name) {
     if (actual == expected) {
         test_pass("%s successful.\n", test_name);
