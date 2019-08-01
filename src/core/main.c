f47a6f30ce1fd40e50ecea98642650d82a64f9b9
Cat <catalinajuarros@gmail.com>
2019-08-01 18:17:31 -0300

    Process CLI arguments and record data

--- src/core/main.c
+++ src/core/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "bcrypt.h"
+#include "bcrypt_constants.h"
 
 #define DEFAULT_N_PASSWORDS 1024
+
+#define ERR_ARGS 0x20010
 
 int main(int argc, char const *argv[]) {
     // Process arguments
@@ -12,16 +16,34 @@ int main(int argc, char const *argv[]) {
     size_t n_passwords;
     char *end;
 
+    strncpy(record, argv[1], BCRYPT_RECORD_SIZE);
+    strncpy(filename, argv[2], strlen(argv[2]));
+
     switch(argc) {
-        case(2):
+        case(3):
             n_passwords = DEFAULT_N_PASSWORDS;
             break;
-        case(3):
-            n_passwords = strtoul(argv[2], &end, 10);
+        case(4):
+            n_passwords = strtoul(argv[3], &end, 10);
             break;
         default:
-            return EXIT_FAILURE;
+            fprintf(stderr, "Usage: cracker <RECORD> <PATH_TO_WORDLIST> <N_PASSWORDS>\n");
+            return ERR_ARGS;
             break;
+    }
+
+    
+    // Process record parameters
+    char record_ciphertext[BCRYPT_HASH_BYTES-3];
+    char salt[BCRYPT_SALT_BYTES];
+    uint64_t rounds;
+
+    int status = get_record_data(&record, &record_ciphertext, &salt, &rounds);
+    if (status) {
+        // Error processing record
+        fprintf(stderr, "Error: get_record_data returned status %x.\n",
+                status);
+        return status;
     }
 
 
