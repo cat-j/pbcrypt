9b91805fd1c7a4539fec167d44e0e28e9cd53a84
Cat <catalinajuarros@gmail.com>
2019-08-03 13:43:19 -0300

    Null-terminate strings and print info

--- src/core/encrypt.c
+++ src/core/encrypt.c
@@ -15,13 +15,21 @@ int main(int argc, char const *argv[]) {
     char salt[BCRYPT_SALT_BYTES+1];
     uint64_t rounds;
     uint8_t log_rounds;
+    size_t length = strlen(argv[1]);
 
-    strncpy(&password, argv[1], strlen(argv[1]));
+    strncpy(&password, argv[1], length);
+    password[length] = 0;
+
     strncpy(&salt, argv[2], BCRYPT_SALT_BYTES);
+    salt[BCRYPT_SALT_BYTES] = 0;
+    
     log_rounds = atoi(argv[3]);
     rounds = 1U << log_rounds;
 
-    char *encrypted = bcrypt(&salt, &password, strlen(password), rounds);
+    printf("%s\n", password);
+    printf("%d\n", rounds);
+
+    char *encrypted = bcrypt(&salt, &password, length, rounds);
     if (encrypted) {
         printf("%s\n", encrypted);
         free(encrypted);
