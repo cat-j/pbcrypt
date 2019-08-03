02b919ced6277418a703bdac7f2006b0071c5a41
Cat <catalinajuarros@gmail.com>
2019-08-03 13:45:17 -0300

    Remove unused files

--- include/utils/file.h
+++
@@ -1,8 +0,0 @@
-#ifndef _FILE_H_
-#define _FILE_H_
-
-#include <stdio.h>
-
-int read_pass_length(FILE *wl_stream, size_t *dst);
-
-#endif
--- src/utils/file.c
+++
@@ -1,7 +0,0 @@
-#include "file.h"
-
-#include <stdio.h>
-
-int read_pass_length(FILE *wl_stream, size_t *dst) {
-    char buf[24];
-}
