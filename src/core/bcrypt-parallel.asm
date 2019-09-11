5df44205b4556e31c9168000d59da222903ce8f1
Cat <catalinajuarros@gmail.com>
2019-09-11 11:00:47 -0300

    Correctly perform enciphering rounds 0 to 15

--- src/core/bcrypt-parallel.asm
+++ src/core/bcrypt-parallel.asm
@@ -66,8 +66,16 @@ blowfish_encipher_parallel:
 
         ; n is even and ranges 2 to 14
         ; n+1 is odd and ranges 3 to 15
-        ; %rep 7
-        ; %endrep
+        %assign i 1
+        %rep 7
+            vmovdqa p_y, [rdi + P_BLF_CTX_P_OFFSET + i*YMM_SIZE] ; | 4 P[n]s | 4 P[n+1]s |
+            ROTATE_128(p_y)
+            movdqa  p_r, p_l ; p_r = 4 P[1]s
+            ROTATE_128(p_y)  ; p_l = 4 P[0]s
+
+            BLOWFISH_ROUND_XMM rdi, p_l, x_l, x_r, f_out, tmp1, tmp2, mask
+            BLOWFISH_ROUND_XMM rdi, p_r, x_r, x_l, f_out, tmp1, tmp2, mask
+        %endrep
 
     .end:
         pop rbp
