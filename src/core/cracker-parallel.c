0e04466fb569ca199ffb8e1dc88b43b1d0d0e11d
Cat <catalinajuarros@gmail.com>
2019-09-26 23:22:37 -0300

    Start main parallel cracker function

--- src/core/cracker-parallel.c
+++ src/core/cracker-parallel.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "cracker-common.h"
 
 /*
@@ -8,4 +10,33 @@
  * If MEASURE_TIME is set, it will also store some measurements
  * to a .csv file, which is useful for experiments.
  */
-int main(int argc, char const *argv[]) {}
+int main(int argc, char const *argv[]) {
+    load_config();
+
+    /////// Process arguments ///////
+
+    int status = process_args(argc, argv);
+    if (status) {
+        fprintf(stderr, "Error: process_args returned status %x.\n",
+                status);
+        return status;
+    }
+
+    /////// Process record parameters ///////
+    
+    uint8_t record_ciphertext[BCRYPT_HASH_BYTES-3];
+    char salt[BCRYPT_SALT_BYTES+1];
+    uint64_t rounds;
+
+    status = get_record_data((char *) &record, (uint8_t *) &record_ciphertext,
+        (uint8_t *) &salt, &rounds);
+    if (status) {
+        // Error processing record
+        fprintf(stderr, "Error: get_record_data returned status %x.\n",
+                status);
+        return status;
+    }
+
+    salt[BCRYPT_SALT_BYTES] = 0; // for pretty printing
+    print_record_info();
+}
