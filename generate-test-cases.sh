aaf7e525962bb9529024a73b53d80db05d2670a3
Cat <catalinajuarros@gmail.com>
2019-10-05 00:19:35 -0300

    Modify boundary and generate fewer/more accurate test cases

--- generate-test-cases.sh
+++ generate-test-cases.sh
@@ -4,20 +4,25 @@ PASSWORD="Go Landcrabs!"
 
 varying_length() {
     local length=$1
-    for i in {4..65540..4}
+    for i in {16..65536..16}
     do
         python ./scripts/generate-wordlist.py "$PASSWORD" $length $i
     done
 }
 
-for i in {1..12}
-do
-    varying_length $i
-done
+generate_varying_length() {
+    for i in {3..12}
+    do
+        varying_length $i
+    done
 
-for i in {39, 52, 65}
-do
-    varying_length $i
-done
+    for i in {13..65..13}
+    do
+        varying_length $i
+    done
 
-varying_length 72
+    varying_length 72
+}
+
+generate_varying_length
+
