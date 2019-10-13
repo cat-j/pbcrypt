f2df5c39e06fa4981a343d92c229c6be5bedfa59
Cat <catalinajuarros@gmail.com>
2019-10-13 14:04:23 -0300

    Generate test cases for password length experiment

--- scripts/generate-test-cases.sh
+++ scripts/generate-test-cases.sh
@@ -2,11 +2,24 @@
 
 PASSWORD="Go Landcrabs!"
 
+
+# Function definitions
+
+# $1: password length
 growing_wordlist() {
     local length=$1
     for i in {32..8192..32}
     do
         python ./scripts/generate-wordlist.py "$PASSWORD" $length $i
+    done
+}
+
+# $1: wordlist size in bytes
+growing_password() {
+    for i in {6..72..6}
+    do
+        local passwords=$(($1 / $i))
+        python ./scripts/generate-wordlist.py "$PASSWORD" $((i - 1)) $passwords
     done
 }
 
@@ -16,6 +29,15 @@ generate_growing_wordlist() {
     growing_wordlist 72
 }
 
+generate_growing_password() {
+    # make sure it's divisible by 6, 12, ... , 72 and fairly big
+    WL_SIZE=$((5*7*11*72*72))
+    growing_password $WL_SIZE
+}
+
+
+# Generate test cases
+
 if [ ! -d ./experiments/test-cases/ ]
 then
     echo "Creating test case directory..."
@@ -23,3 +45,5 @@ then
 fi
 
 generate_growing_wordlist
+
+generate_growing_password
